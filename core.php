de0434c2f22d7f839c93136ce223059e329dbcef
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2013-05-05 22:21:02 +0300

    Integration with WP_Users_List_Table

--- core.php
+++ core.php
@@ -34,8 +34,11 @@ class EM_LogInAs
 
 	public function admin_init()
 	{
-		add_action('load-em_log_in_as/pages/loginas.php', array($this, 'load_loginas_page'));
-		add_action('admin_post_login_as',                 array($this, 'do_login_as'));
+		if (current_user_can('manage_options')) {
+			add_action('load-em_log_in_as/pages/loginas.php', array($this, 'load_loginas_page'));
+			add_action('admin_post_login_as',                 array($this, 'admin_post_login_as'));
+			add_action('user_row_actions',                    array($this, 'user_row_actions'), 10, 2);
+		}
 	}
 
 	public function admin_menu()
@@ -59,16 +62,23 @@ class EM_LogInAs
 		);
 	}
 
-	public function do_login_as()
+	public function admin_post_login_as()
 	{
 		check_admin_referer('login-as');
 
-		$id = intval($_POST['userid']);
+		$id = isset($_POST['userid']) ? intval($_POST['userid']) : intval($_GET['userid']);
 		wp_logout();
 		wp_set_auth_cookie($id, false, (is_ssl() ? true : false));
 
 		wp_redirect(admin_url());
 		die();
+	}
+
+	public function user_row_actions(array $actions, WP_User $user)
+	{
+		$link = wp_nonce_url(admin_url("admin-post.php?action=login_as&userid={$user->ID}"), 'login-as');
+		$actions['loginas'] = "<a href='{$link}'>" . sprintf(__('Log In As <strong>%s</strong>', 'emloginas'), $user->user_login) . '</a>';
+		return $actions;
 	}
 }
 
